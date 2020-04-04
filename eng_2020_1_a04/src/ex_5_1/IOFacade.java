99ee6f34a28421c67a8df0d33605ac551f8fa408
user <v.felicio@hotmail.com>
2020-04-04 13:47:09 -0300

    Atividade 04 Versão 3.1

--- eng_2020_1_a04/src/ex_5_1/IOFacade.java
+++ eng_2020_1_a04/src/ex_5_1/IOFacade.java
@@ -1,28 +1,24 @@
 package ex_5_1;
 
-public class IOFacade {
-
-	LerArquivoTexto txt = new LerArquivoTexto();
-	LerArquivoBinario bn = new LerArquivoBinario();
-	LerArquivoObjeto obj = new LerArquivoObjeto();
-	String stxt = "";
-	String sbn = "";
-	String sobj = "";
-	
-	
-	
+public class IOFacade {	
 public void lerTxt() {
 	//le o aluno.txt
+	LerArquivoTexto txt = new LerArquivoTexto();
+	String stxt = "";
 	stxt = txt.lerArquivoTxt();
 	System.out.println(stxt+"\n");
 }
 public void lerBinario() {
 	//le o Aluno.class (devolve os bytes)
+	LerArquivoBinario bn = new LerArquivoBinario();
+	String sbn = "";
 	sbn = bn.lerArquivoBinario();
 	System.out.println(sbn+"\n");
 }
 public void lerObjeto() {
 	//le o aluno (objeto)
+	LerArquivoObjeto obj = new LerArquivoObjeto();
+	String sobj = "";
 	sobj = obj.lerArquivoObjeto();
 	System.out.println(sobj+"\n");
 }
